88ee3555fbcd7e33bae5f884da3868257fbd2d3e
Sindhu-devi196 <96579718+Sindhu-devi196@users.noreply.github.com>
2021-12-23 18:38:56 +0500

    complete Calculator

--- calculator.py
+++ calculator.py
@@ -15,3 +15,13 @@ elif operation == 2:
     num1 = int(input("Enter your 1st value: "))
     num2 = int(input("Enter your 2nd value: "))
     print("The substration of", num1,"and" ,num2 ," is ",int(num1-num2))
+elif operation == 3:
+    num1 = int(input("Enter your 1st value: "))
+    num2 = int(input("Enter your 2nd value: "))
+    print("The multiply of", num1,"and" ,num2 ," is ",int(num1*num2))
+elif operation == 4 :
+    num1 = int(input("Enter your 1st value: "))
+    num2 = int(input("Enter your 2nd value: "))
+    print("The divide of", num1,"and" ,num2 ," is ",int(num1/num2))
+else:
+    print("Invalid operation you choose please try again!")
